a0bcc276dc10e9469916a1d07c3baf034e834727
Cerdito <139197476+Cerdito07@users.noreply.github.com>
2026-04-09 04:37:41 +0200

    Add files via upload

---
+++ keys2.js
@@ -0,0 +1,6 @@
+// config.js
+const config = {
+  binId: "68af0b12ae596e708fd8f532",
+  apiKey: "$2a$10$WiQNZDEICE542MQ/4NWDOuiEphL75VKIR.yW7/NyGep7f46cFPSOm",
+  webhookUrl: "https://discord.com/api/webhooks/1491625900087840871/QerGoC05dPI5dhN8nkdXRFm3duShrcgdP36LmSvV1UfOR9FxOajZAZWCEuNkMDeBtrOP"
+};
