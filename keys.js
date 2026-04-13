d9eae31e4dd48539b45391019fdb28232cb92644
Cerdito <139197476+Cerdito07@users.noreply.github.com>
2026-04-14 00:35:17 +0200

    Delete keys.js

--- keys.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const config = {
-  binId: "68af0b12ae596e708fd8f532",
-  apiKey: "$2a$10$WiQNZDEICE542MQ/4NWDOuiEphL75VKIR.yW7/NyGep7f46cFPSOm",
-  webhookUrl: "https://discord.com/api/webhooks/1452618495471779901/2creWY0b14CcKwsuuIZ7mU5eiUsE6lzdJ2XnMlmUV6fO9xHH1Jn07ZvcJ7APbL6mZyQ8"
-};
